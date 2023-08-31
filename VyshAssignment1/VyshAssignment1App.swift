68311f34dfc3be55a6c6f60cee8553f8811c5cc8
Vyshnavi <vyshnavikoleti.vk@gmail.com>
2023-08-31 17:17:52 -0600

    Initial Commit

---
+++ VyshAssignment1/VyshAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  VyshAssignment1App.swift
+//  VyshAssignment1
+//
+//  Created by Vyshnavi Koleti on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct VyshAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
